25d1c6b0e5590b53c2fdd54e6223818847401d01
zairgb <jairmjia.gb@gmail.com>
2026-06-19 11:12:50 -0600

    componentes

---
+++ src/app/Components/main/main.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  imports: [],
+  templateUrl: './main.html',
+  styleUrl: './main.css',
+})
+export class Main {}
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './Components/header/header';
+import { Main } from './Components/main/main';
+import { Footer } from "./Components/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Main, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
